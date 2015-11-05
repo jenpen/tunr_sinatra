d043802fc86aa3ad07019da38bdd6e7404b6c45f
jen pengelly <jgpengelly@gmail.com>
2015-11-05 10:28:19 -0500

    update app.rb to include comments for each function. setup css. setup controllers.

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'bundler/setup'
+
+# Gems
 require 'pg'
 require 'active_record'
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
+# load database
 require_relative 'db/connection'
+
+# load controllers
+require_relative 'controllers/artists_controller.rb'
+
+# load models
 require_relative 'models/artist'
 require_relative 'models/song'
 
-
+# routes
 get '/' do
   erb :home
 end
