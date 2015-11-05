efb7e045845f65f0cfb403acefc362e4b0ea8da9
jen pengelly <jgpengelly@gmail.com>
2015-11-05 11:17:17 -0500

    update artist/index to iterate thru artists. Add styling for no bullets. Update layout with content section.

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -1,3 +1,14 @@
+# Index (index all artists) - GET "/artists"
 get "/artists" do
-  "Hello World"
+  puts " 🌟 " * 12
+  @artists = Artist.all
+  erb(:"artists/index")
 end
+
+
+# Show (show particular artist) - GET "/artist/:id"
+# New (render form to create new artists) - GET "/artists/new"
+# Edit (render form to edit existing artist) - GET "/artists/:id/edit"
+# Create (submit form to create new artist) - POST "/artists"
+# Update (submit form to update existing artist) - PUT "/artists/:id"
+# Destroy (delete an existing artist) - DELETE "/artists/:id"
