31701f5416b43d845097d2fe27da6ef951351fcd
jen pengelly <jgpengelly@gmail.com>
2015-11-05 16:31:18 -0500

    update to include edit and create. Rb created for Delete, but need to place in show.erb

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -12,19 +12,33 @@ end
 # Edit (render form to edit existing artist) - GET "/artists/:id/edit"
 get "/artists/:id/edit" do
   puts " 🌟 " * 12
-  @edit_this_artist = Artist.find(params[:id])
+  @artist = Artist.find(params[:id])
   erb :"artists/edit"
 end
 
 # Show (show particular artist) - GET "/artist/:id"
 get "/artists/:id" do
-  @each_artist = Artist.find(params[:id])
+  @artist = Artist.find(params[:id])
   erb :"artists/show"
 end
 
-
-
-
 # Create (submit form to create new artist) - POST "/artists"
+post "/artists" do
+  @artist = Artist.create(params[:artist])
+  redirect("/artists")
+end
+
+
 # Update (submit form to update existing artist) - PUT "/artists/:id"
+post "/artists/:id" do
+  @artist = Artist.find(params[:id])
+  @artist.update(params[:artist])
+  redirect("/artists/#{@artist.id}")
+end
+
 # Destroy (delete an existing artist) - DELETE "/artists/:id"
+# delete "/artists/:id" do
+#   @artist = Artist.find(params[:id])
+#   @artist.update(params[:artist])
+#   redirect("/artists/#{@artist.id}")
+# end
