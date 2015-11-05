74c301b184bab9cde6df3deb24b7bf95236a43a6
jen pengelly <jgpengelly@gmail.com>
2015-11-05 09:36:54 -0500

    Initial commit to new branch. Lab-starter code incorporated and brings in files from non-git page

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'bundler/setup'
+require 'pg'
+require 'active_record'
+require 'pry'
+
+require_relative 'db/connection'
+require_relative 'models/artist'
+require_relative 'models/song'
+
+binding.pry
