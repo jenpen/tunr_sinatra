92cfe77b4724d83a460bbe24001526b928ee7995
jen pengelly <jgpengelly@gmail.com>
2015-11-05 09:51:02 -0500

    Update Gemfiles to include sinatra. Update app.rb with route. Create views and erb files, layout and home.

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'bundler/setup'
 require 'pg'
 require 'active_record'
 require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
 
 require_relative 'db/connection'
 require_relative 'models/artist'
 require_relative 'models/song'
 
-binding.pry
+
+get '/' do
+  erb :home
+end
